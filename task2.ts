981075d57d5559a38f8984a8da16d527036f66ce
renaldin1 <renaldinoviandi0@gmail.com>
2025-05-07 06:05:46 +0700

    first commit

--- task2.ts
+++ task2.ts
@@ -1,16 +1,12 @@
 const nim = "054592243"
 const lastTwoNim = parseInt(nim.slice(-2))
-
 let first: number
-
 if (lastTwoNim > 20) {
   first = parseInt(nim.slice(-1))
 } else {
   first = lastTwoNim
 }
-
 const lastNim = parseInt(nim.slice(-1))
-
 if (lastNim % 2 !== 0) {
   let i = first
   while (i <= 20) {
